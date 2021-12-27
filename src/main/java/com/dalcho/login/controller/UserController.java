320eece39f38c0368c7fc68e06ab00e12b6805a8
Lluroa <luora@naver.com>
2021-12-28 00:12:33 +0900

    [add] 회원가입 및 로그인 [#1]

---
+++ src/main/java/com/dalcho/login/controller/UserController.java
@@ -0,0 +1,48 @@
+package com.dalcho.login.controller;
+
+import com.dalcho.login.dto.SignupRequestDto;
+import com.dalcho.login.model.User;
+import com.dalcho.login.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class UserController {
+
+    private final UserService userService;
+
+    // userService 생성자 생성
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+
+    // 회원 로그인 페이지
+    @GetMapping("/user/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/user/login/error")
+    public String loginError(Model model) {
+        model.addAttribute("loginError", true);
+        return "login";
+    }
+
+    // 회원 가입 페이지
+    @GetMapping("/user/signup")
+    public String signup() {
+        return "signup";
+    }
+
+    // 회원 가입 요청 처리
+    @PostMapping("/user/signup")
+    public String registerUser(SignupRequestDto requestDto) {
+        userService.registerUser(requestDto);
+        return "redirect:/";
+    }
+}
---
+++ src/main/java/com/dalcho/login/model/UserRole.java
@@ -0,0 +1,6 @@
+package com.dalcho.login.model;
+
+public enum UserRole {
+    USER,  // 사용자 권한
+    ADMIN  // 관리자 권한
+}
