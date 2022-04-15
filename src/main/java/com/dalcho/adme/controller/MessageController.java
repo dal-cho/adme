ad0ea76b8415529a48a2116e803af9b06138bff2
Lluroa <luora@naver.com>
2022-04-15 23:37:13 +0900

    [add] 웹 소켓 코드 추가 [#29]

--- src/main/java/com/dalcho/adme/controller/MessageController.java
+++ src/main/java/com/dalcho/adme/controller/MessageController.java
@@ -5,18 +5,22 @@ import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
 import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 import javax.websocket.*;
 import javax.websocket.server.ServerEndpoint;
 import java.io.IOException;
-import java.net.Socket;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 @Controller
 @ServerEndpoint("/websocket")
-public class MessageController extends Socket {
+//websocket 핸들러를 구현하기 위해서 기본적으로 TextWebSocketHandler를 상속
+public class MessageController extends TextWebSocketHandler {
     private static final Logger logger = LoggerFactory.getLogger(MessageController.class);
+    private static List<Session> sessionUsers = Collections.synchronizedList(new ArrayList<>());
 
     private static final List<Session> session = new ArrayList<Session>();
 
@@ -31,6 +35,7 @@ public class MessageController extends Socket {
     public void open(Session newUser) {
         System.out.println("connected");
         session.add(newUser);
+        sessionUsers.add(newUser);
         System.out.println(newUser.getId());
     }     // 사용자가 증가할 때마다 세션의 getId()는 1씩 증가하며 문자열 형태로 지정된다.
 
@@ -38,13 +43,13 @@ public class MessageController extends Socket {
     @OnMessage // 사용자로부터 메시지를 받았을 때, 실행된다.
     public void getMsg(String msg) {
         for (int i = 0; i < session.size(); i++) {
-                try {
-                    session.get(i).getBasicRemote().sendText(msg); 
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+            try {
+                session.get(i).getBasicRemote().sendText(msg);
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         }
+    }
 
 
     @Bean
@@ -55,13 +60,15 @@ public class MessageController extends Socket {
 
     @OnClose
     public void onClose(Session session, CloseReason closeReason) {
+        sessionUsers.remove(session);
+        System.out.println("remove : " + session);
         logger.info(String.format("Session %s closed because of %s", session.getId(), closeReason));
     }
 
     //에러 발생시
     @OnError
     public void onError(Session session, Throwable e) {
-        logger.info("문제 세션 : "+ session);
+        logger.info("문제 세션 : " + session);
         System.out.println(e.toString());
     }
 }
