b285e7227b3ecc0e5831c336735c6a9799420290
Lluroa <luora@naver.com>
2022-05-21 22:33:42 +0900

    [add] 페이징 코드 추가 [#45]

--- src/main/java/com/dalcho/adme/service/RegistryService.java
+++ src/main/java/com/dalcho/adme/service/RegistryService.java
@@ -23,6 +23,13 @@ public class RegistryService {
 //    private static final int BLOCK_PAGE_NUM_COUNT = 5; // 블럭에 존재하는 페이지 번호 수
     private static final int PAGE_POST_COUNT = 6; // 한 페이지에 존재하는 게시글 수
 
+    private int displayPageNum = 5; // 화면 하단에 보여지는 페이지 버튼의 수
+    private int startPage; //  화면에 보여지는 페이징 5개 중 시작 페이지 번호
+    private int endPage; // 화면에 보여지는 페이징 5개 중 마지막 페이지 번호, 끝 페이지 번호
+    private boolean prev; // 이전 버튼 생성 여부
+    private boolean next; // 다음 버튼 생성 여부
+    private int nowPage; // 현재 페이지
+
     // 게시글 등록
     @Transactional
     public Registry setUpload(RegistryDto registryDto) throws IOException {
@@ -38,7 +45,20 @@ public class RegistryService {
 
         Page<Registry> boards = registryRepository.findAllByOrderByCreatedAtDesc(pageable);// 생성 날짜 순으로 보여주기
         List<Registry> boardList = boards.getContent(); // 조회된 데이터
-        return new PagingResult(boardList, boards.getTotalPages()); // 조회된 데이터, 전체 페이지의 수
+
+        nowPage = boards.getNumber()+1;
+        startPage = (int) Math.ceil( nowPage / (double) displayPageNum ) *5+1;
+        endPage = (int) (Math.ceil(displayPageNum / (double) displayPageNum) * displayPageNum);
+        prev = startPage == 1 ? false : true; // 이전 버튼 생성 여부
+
+        // next 수정 필요
+        next = endPage > boards.getTotalPages();
+        if(endPage > boards.getTotalPages()) {
+            endPage = boards.getTotalPages();
+            next = false;
+        }
+
+        return new PagingResult(boardList, boards.getTotalPages(), startPage, endPage, prev, next, displayPageNum);
     }
 
 
