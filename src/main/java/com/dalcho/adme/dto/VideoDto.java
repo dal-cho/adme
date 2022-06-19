148ca0548e79af00803f1def0e4daafd8c575cd7
Yu_Jehyeop <fbwpguq@gmail.com>
2022-06-20 00:43:11 +0900

    [add] file 업로드(이미지) 로컬에 저장[#34]

--- src/main/java/com/dalcho/adme/dto/VideoDto.java
+++ src/main/java/com/dalcho/adme/dto/VideoDto.java
@@ -1,12 +1,13 @@
 package com.dalcho.adme.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
 @AllArgsConstructor
-@Getter
+@Data
 public class VideoDto {
     // 비디오 URL(이미지 관리 방식으로 진행중)
     private String video;
@@ -16,4 +17,10 @@ public class VideoDto {
     private String comment;
     // 비디오에 설정한 태그
     private String tag;
+
+    // 이미지 업로드용
+    private String fileName;
+    private String uploadPath;
+    private String uuid;
+    private boolean image;
 }
