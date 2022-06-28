9e247599a7df196aa99081e902f861579a1369ae
Yu_Jehyeop <fbwpguq@gmail.com>
2022-06-29 04:02:19 +0900

    [fix] file 업로드 controller 코드 수정 및 service, repository 부분 작성 [#81]

--- src/main/java/com/dalcho/adme/dto/VideoDto.java
+++ src/main/java/com/dalcho/adme/dto/VideoDto.java
@@ -1,26 +1,11 @@
 package com.dalcho.adme.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
 
-@NoArgsConstructor
-@AllArgsConstructor
 @Data
 public class VideoDto {
-    // 비디오 URL(이미지 관리 방식으로 진행중)
-    private String video;
-    // 비디오 제목
-    private String title;
-    // 비디오 설명
-    private String comment;
-    // 비디오에 설정한 태그
-    private String tag;
-
-    // 이미지 업로드용
     private String fileName;
-    private String uploadPath;
     private String uuid;
+    private String uploadPath;
     private boolean image;
 }
