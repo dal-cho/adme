771711d70ead0eca570cd5db36b0c86148e4b1d9
soyeon <luora@naver.com>
2022-12-31 01:09:21 +0900

    [add] 고객센터 파일 저장 후 읽어내기 확인 [#136]

--- src/main/java/com/dalcho/adme/Impl/ChatBotServiceImpl.java
+++ src/main/java/com/dalcho/adme/Impl/ChatBotServiceImpl.java
@@ -1,5 +1,6 @@
 package com.dalcho.adme.Impl;
 
+import com.google.gson.Gson;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
@@ -11,6 +12,9 @@ import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 import javax.websocket.*;
 import javax.websocket.server.ServerEndpoint;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -22,6 +26,8 @@ import java.util.List;
 @ServerEndpoint("/chatbot")
 public class ChatBotServiceImpl extends TextWebSocketHandler {
     private static List<Session> sessionUsers = Collections.synchronizedList(new ArrayList<>());
+
+    private Gson gson = new Gson(); // JSON을 파싱하는 클래스
 
     @OnOpen
     public void open(Session newUser) throws IOException {
@@ -45,6 +51,53 @@ public class ChatBotServiceImpl extends TextWebSocketHandler {
                     e.printStackTrace();
                 }
             }
+        }
+        ChatMessage message = gson.fromJson(msg, ChatMessage.class);
+        // State가 0이라면 초기 접속
+        if (message.getState() == 0) {
+            // 세션 만들기
+            try {
+                // 파일로부터 채팅 내용을 읽어와서 보내기
+                receiveSession.getBasicRemote().sendText(readFile());
+            } catch (Throwable e) {
+                // 에러가 발생할 경우.
+                e.printStackTrace();
+            }
+            // State가 1이라면 일반 메시지
+        } else if (message.getState() == 1) {
+            // 세션 확인 하기
+            if (receiveSession != null) {
+                // 파일에 저장하기
+                saveFile(message.getId(), message.getValue());
+            }
+        }
+    }
+
+    // 채팅 내용을 파일로 부터 읽어온다.
+    private String readFile() {
+        File file = new File("C:\\Users\\cjera\\Desktop\\달코\\chat\\chat.txt");
+        // 파일 있는지 검사
+        if (!file.exists()) {
+            return "";
+        }
+        // 파일을 읽어온다.
+        try (FileInputStream stream = new FileInputStream(file)) {
+            return new String(stream.readAllBytes());
+        } catch (Throwable e) {
+            e.printStackTrace();
+            return "";
+        }
+    }
+
+    // 파일를 저장하는 함수
+    private void saveFile(String id, String message) {
+        // 메시지 내용
+        String msg = id + ":  " + message + "\n";
+        // 파일을 저장한다.
+        try (FileOutputStream stream = new FileOutputStream("C:\\Users\\cjera\\Desktop\\달코\\chat\\chat.txt", true)) {
+            stream.write(msg.getBytes("UTF-8"));
+        } catch (Throwable e) {
+            e.printStackTrace();
         }
     }
 
@@ -70,4 +123,37 @@ public class ChatBotServiceImpl extends TextWebSocketHandler {
 
     }
 
+    class ChatMessage {
+        // id
+        private String id;
+        // state
+        private int state;
+        // 내용
+        private String value;
+
+        // getter, setter
+        public String getId() {
+            return id;
+        }
+
+        public void setId(String id) {
+            this.id = id;
+        }
+
+        public int getState() {
+            return state;
+        }
+
+        public void setState(int state) {
+            this.state = state;
+        }
+
+        public String getValue() {
+            return value;
+        }
+
+        public void setValue(String value) {
+            this.value = value;
+        }
+    }
 }
