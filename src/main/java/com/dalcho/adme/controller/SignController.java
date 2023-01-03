16e7a0ea16d22d2bb933f7eaabbdf175dee2c066
Yu_Jehyeop <fbwpguq@gmail.com>
2023-01-03 17:49:31 +0900

    [fix] Security 검토 및 수정 [#145]

--- src/main/java/com/dalcho/adme/controller/SignController.java
+++ src/main/java/com/dalcho/adme/controller/SignController.java
@@ -7,17 +7,14 @@ import com.dalcho.adme.dto.sign.SignUpResultDto;
 import com.dalcho.adme.service.SignService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 @Slf4j
 @RestController
--- src/main/java/com/dalcho/adme/service/UserDetailsService.java
+++
@@ -1,9 +0,0 @@
-package com.dalcho.adme.service;
-
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-
-public interface UserDetailsService {
-    // UserDetails = User (구현체)
-    UserDetails loadUserByUsername(String username) throws UsernameNotFoundException;
-}
