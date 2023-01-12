5ad431a2e92a84ddd0cf6d43f73743ad404f7c87
soyeon <luora@naver.com>
2023-01-12 23:48:29 +0900

    [update] 어노테이션 수정 [#162]

--- src/main/java/com/dalcho/adme/exception/CustomException.java
+++ src/main/java/com/dalcho/adme/exception/CustomException.java
@@ -1,20 +1,13 @@
 package com.dalcho.adme.exception;
 
+import lombok.Getter;
+
+@Getter
 public class CustomException extends RuntimeException {
     private static final long serialVersionUID = 4663380430591151694L;
-
     private final ErrorCode errorCode;
     public CustomException(ErrorCode errorCode) {
         super(errorCode.getMessage());
         this.errorCode = errorCode;
-    }
-
-    public CustomException(final ErrorCode errorCode, final String message) {
-        super(message);
-        this.errorCode = errorCode;
-    }
-
-    public ErrorCode getErrorCode() {
-        return errorCode;
     }
 }
--- src/main/java/com/dalcho/adme/exception/CustomExceptionHandler.java
+++ src/main/java/com/dalcho/adme/exception/CustomExceptionHandler.java
@@ -3,10 +3,10 @@ package com.dalcho.adme.exception;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class CustomExceptionHandler {
     private final Logger LOGGER = LoggerFactory.getLogger(CustomExceptionHandler.class); // Logger를 등록
 
