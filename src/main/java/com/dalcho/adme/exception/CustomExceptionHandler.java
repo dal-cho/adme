5910d77a1bb0bc1a4a60b49607e4840d293cd6af
soyeon <luora@naver.com>
2023-01-24 21:16:06 +0900

    [add] referenceById exception 추가 [#182]

--- src/main/java/com/dalcho/adme/exception/CustomExceptionHandler.java
+++ src/main/java/com/dalcho/adme/exception/CustomExceptionHandler.java
@@ -4,6 +4,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.sql.SQLException;
+
 @RestControllerAdvice
 public class CustomExceptionHandler {
     //private final Logger LOGGER = LoggerFactory.getLogger(CustomExceptionHandler.class); // Logger를 등록
@@ -12,4 +14,10 @@ public class CustomExceptionHandler {
     protected ResponseEntity<ErrorResponseEntity> handleCustomException(CustomException e) {
         return ErrorResponseEntity.toResponseEntity(e.getErrorCode());
     }
+
+    @ExceptionHandler(SQLException.class)
+    private ResponseEntity<ErrorResponseEntity> handleSQLException(){
+        return ErrorResponseEntity.toResponseEntity(ErrorCode.DATABASE_ERROR);
+    }
+
 }
