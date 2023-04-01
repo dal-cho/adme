43326f21a71bd76cd67065eaaabcc692471706d4
soyeon <luora@naver.com>
2023-04-02 00:27:00 +0900

    [update] 관리자 채팅 알림 msg [#199]

--- src/main/java/com/dalcho/adme/service/Impl/ChatServiceImpl.java
+++ src/main/java/com/dalcho/adme/service/Impl/ChatServiceImpl.java
@@ -1,17 +1,16 @@
 package com.dalcho.adme.service.Impl;
 
-import com.dalcho.adme.domain.Socket;
+import com.dalcho.adme.domain.Chat;
 import com.dalcho.adme.dto.chat.ChatMessage;
 import com.dalcho.adme.dto.chat.ChatRoomDto;
 import com.dalcho.adme.dto.chat.ChatRoomMap;
-import com.dalcho.adme.exception.CustomException;
 import com.dalcho.adme.exception.notfound.FileNotFoundException;
-import com.dalcho.adme.exception.notfound.ChatRoomNotFoundException;
 import com.dalcho.adme.repository.ChatRepository;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.input.ReversedLinesFileReader;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
@@ -22,6 +21,7 @@ import javax.annotation.PostConstruct;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.NoSuchFileException;
 import java.nio.file.Paths;
@@ -33,21 +33,25 @@ import java.util.*;
 public class ChatServiceImpl {
 	private final ChatRepository chatRepository;
 	private Map<String, Integer> connectUsers;
+	private Map<String, Integer> adminChat;
+	private Map<String, Integer> userChat;
 	@Value("${spring.servlet.multipart.location}")
 	private String chatUploadLocation;
 
 	@PostConstruct
-	private void setUp() { // 안그러면 NullPointerException
+	private void setUp() {
 		this.connectUsers = new HashMap<>();
+		this.adminChat = new HashMap<>();
+		this.userChat = new HashMap<>();
 	}
 
 	//채팅방 불러오기
 	public List<ChatRoomDto> findAllRoom() {
 		List<ChatRoomDto> chatRoomDtos = new ArrayList<>();
-		List<Socket> all = chatRepository.findAll();
+		List<Chat> all = chatRepository.findAll();
 		try {
 			for (int i = 0; i < all.size(); i++) {
-				chatRoomDtos.add(ChatRoomDto.of(all.get(i)));
+				chatRoomDtos.add(ChatRoomDto.of(all.get(i), lastLine(all.get(i).getRoomId())));
 			}
 		} catch (NullPointerException e) {
 			throw new RuntimeException("data 없음! ");
@@ -61,13 +65,13 @@ public class ChatServiceImpl {
 		if (!chatRepository.existsByNickname(nickname)) {
 			chatRoom = ChatRoomDto.create(nickname);
 			ChatRoomMap.getInstance().getChatRooms().put(chatRoom.getRoomId(), chatRoom);
-			Socket socket = new Socket(chatRoom.getRoomId(), nickname);
-			log.info("Service socket :  " + socket);
-			chatRepository.save(socket);
+			Chat chat = new Chat(chatRoom.getRoomId(), nickname);
+			log.info("Service socket :  " + chat);
+			chatRepository.save(chat);
 			return chatRoom;
 		} else {
-			Optional<Socket> byNickname = chatRepository.findByNickname(nickname);
-			return ChatRoomDto.of(byNickname.get());
+			Optional<Chat> findChat = chatRepository.findByNickname(nickname);
+			return ChatRoomDto.of(findChat.get(), lastLine(findChat.get().getRoomId()));
 		}
 	}
 
@@ -78,6 +82,8 @@ public class ChatServiceImpl {
 		json.put("type", chatMessage.getType().toString());
 		json.put("sender", chatMessage.getSender());
 		json.put("message", chatMessage.getMessage());
+		json.put("adminChat", adminChat.get(chatMessage.getRoomId()));
+		json.put("userChat", userChat.get(chatMessage.getRoomId()));
 		Gson gson = new GsonBuilder().setPrettyPrinting().create();
 		String json1 = gson.toJson(json);
 		try {
@@ -108,6 +114,27 @@ public class ChatServiceImpl {
 			e.printStackTrace();
 			return null;
 		}
+	}
+
+	public List lastLine(String roomId) {
+		File file1 = new File(chatUploadLocation + "/" + roomId + ".txt");
+		try{
+			ReversedLinesFileReader reader
+					= new ReversedLinesFileReader(file1, Charset.forName("UTF-8"));
+
+			List<String> lines = reader.readLines(7);
+
+			List<String> chat = new ArrayList<>();
+			chat.add(lines.get(6).substring(15, lines.get(6).length()-1)); // adminChat
+			chat.add(lines.get(4).substring(14, lines.get(4).length()-1)); // userChat
+			chat.add(lines.get(2).substring(14, lines.get(2).length()-2)); // message
+			return chat;
+		}catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return new ArrayList<>();
 	}
 
 	public ChatMessage chatAlarm(String sender, String roomId){
