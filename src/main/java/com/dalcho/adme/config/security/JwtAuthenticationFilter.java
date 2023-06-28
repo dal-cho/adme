ec57e88d606f44e71c20c2593ef0b53ed3e4d726
soyeon <luora@naver.com>
2023-06-29 00:41:55 +0900

    [update] JwtAuthenticationFilter 로그 제거 [#229]

--- src/main/java/com/dalcho/adme/config/security/JwtAuthenticationFilter.java
+++ src/main/java/com/dalcho/adme/config/security/JwtAuthenticationFilter.java
@@ -13,33 +13,68 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@Slf4j
-@Component
+//@Slf4j
+//@Component
+//@RequiredArgsConstructor
+//public class JwtAuthenticationFilter extends OncePerRequestFilter {
+//
+//    private final JwtTokenProvider jwtTokenProvider;
+//
+//    @Override
+//    public void doFilterInternal(
+//            HttpServletRequest servletRequest,
+//            HttpServletResponse servletResponse,
+//            FilterChain filterChain) throws ServletException, IOException {
+//        // servletRequest 에서 token 추출
+//        String token = jwtTokenProvider.resolveToken(servletRequest);
+//
+//        log.info("[dofilterInternal] token 값 유효성 체크 시작");
+//        // token 이 유효하다면 Authentication 객체를 생성해서 SecurityContextHolder 에 추가
+//        if (token != null && jwtTokenProvider.validateToken(token)) {
+//            Authentication authentication = jwtTokenProvider.getAuthentication(token);
+//            SecurityContextHolder.getContext().setAuthentication(authentication);
+//            log.info("[doFilterInternal] token 값 유효성 체크 완료");
+//        }
+//        else {
+//            log.info("[doFilterInternal] 유효한 JWT 토큰이 없습니다, uri: {}", servletRequest.getRequestURL());
+//        }
+//
+//        // 요청 정보가 매칭되지 않을경우 동작
+//        filterChain.doFilter(servletRequest, servletResponse);
+//    }
+//}
 @RequiredArgsConstructor
+@Slf4j
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
-
     private final JwtTokenProvider jwtTokenProvider;
-
+    private final String BEARER = "Bearer ";
     @Override
-    public void doFilterInternal(
-            HttpServletRequest servletRequest,
-            HttpServletResponse servletResponse,
-            FilterChain filterChain) throws ServletException, IOException {
-        // servletRequest 에서 token 추출
-        String token = jwtTokenProvider.resolveToken(servletRequest);
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String token = request.getHeader("Authorization");
+        request.setAttribute("existsToken", true); // 토큰 존재 여부 초기화
 
-        log.info("[dofilterInternal] token 값 유효성 체크 시작");
-        // token 이 유효하다면 Authentication 객체를 생성해서 SecurityContextHolder 에 추가
-        if (token != null && jwtTokenProvider.validateToken(token)) {
+        if (isEmptyToken(token)) request.setAttribute("existsToken", false); // 토큰이 없는 경우 false로 변경
+
+        if (token == null || !token.startsWith(BEARER)) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        token = parseBearer(token);
+
+        if (jwtTokenProvider.validateToken(token)) {
             Authentication authentication = jwtTokenProvider.getAuthentication(token);
             SecurityContextHolder.getContext().setAuthentication(authentication);
-            log.info("[doFilterInternal] token 값 유효성 체크 완료");
-        }
-        else {
-            log.info("[doFilterInternal] 유효한 JWT 토큰이 없습니다, uri: {}", servletRequest.getRequestURL());
         }
 
-        // 요청 정보가 매칭되지 않을경우 동작
-        filterChain.doFilter(servletRequest, servletResponse);
+        filterChain.doFilter(request, response);
+    }
+
+    private boolean isEmptyToken(String token) {
+        return token == null || "".equals(token);
+    }
+
+    private String parseBearer(String token) {
+        return token.substring(BEARER.length()); //"Bearer "를 제외한 나머지
     }
 }
