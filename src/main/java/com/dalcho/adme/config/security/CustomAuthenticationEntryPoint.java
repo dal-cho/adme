6462d5fd803696103d258b0c51af01b3a2ce7b93
soyeon <luora@naver.com>
2023-07-13 01:20:22 +0900

    [update] 인증 실패 로직 [#229]

--- src/main/java/com/dalcho/adme/config/security/CustomAuthenticationEntryPoint.java
+++ src/main/java/com/dalcho/adme/config/security/CustomAuthenticationEntryPoint.java
@@ -1,10 +1,12 @@
 package com.dalcho.adme.config.security;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
-import org.springframework.security.web.DefaultRedirectStrategy;
-import org.springframework.security.web.RedirectStrategy;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
@@ -13,13 +15,23 @@ import java.io.IOException;
 
 @Slf4j
 @Component
+@RequiredArgsConstructor
 public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    private final ObjectMapper objectMapper;
+    private void sendErrorResponse(HttpServletResponse response, String message) throws IOException {
+        response.setCharacterEncoding("utf-8");
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
 
-
-    private final RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
+        // 수정된 부분: 응답의 내용 설정
+        String json = objectMapper.writeValueAsString(message);
+        response.getWriter().write(json);
+    }
 
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
-        redirectStrategy.sendRedirect(request, response, "/user/login");
+        log.info("[ commence ] : " + "인증 실패");
+        sendErrorResponse(response, "인증 실패");
+        response.sendRedirect("/user/login");
     }
 }
