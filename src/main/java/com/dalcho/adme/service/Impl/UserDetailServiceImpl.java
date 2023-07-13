2e7cb1c9fd8783dc370e6b273c99cae778bb11ac
soyeon <luora@naver.com>
2023-07-13 22:30:58 +0900

    [update] UserDetailsService 수정 [#229]

--- src/main/java/com/dalcho/adme/service/Impl/UserDetailServiceImpl.java
+++ src/main/java/com/dalcho/adme/service/Impl/UserDetailServiceImpl.java
@@ -1,28 +1,43 @@
 package com.dalcho.adme.service.Impl;
 
-import com.dalcho.adme.exception.notfound.UserNotFoundException;
+import com.dalcho.adme.domain.User;
 import com.dalcho.adme.repository.UserRepository;
-
-import com.dalcho.adme.service.UserDetailService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+
+@Slf4j
 @Service
 @RequiredArgsConstructor
-@Slf4j
-public class UserDetailServiceImpl implements UserDetailService {
+public class UserDetailServiceImpl implements UserDetailsService {
 
     private final UserRepository userRepository;
 
-    // User 엔티티의 id 값 가져오기 (인증)
     @Override
-    public UserDetails loadUserByUsername(String username) {
-        log.info("[loadUserByUsername] loadUserByUsername 수행. username : {}", username);
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> user = userRepository.findByNickname(username);
+        if (user.isEmpty()) {
+            throw new UsernameNotFoundException("User not found with username: " + username);
+        }
 
-        return userRepository.findByNickname(username).orElseThrow(() -> {
-            throw new UserNotFoundException();
-        });
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority(user.get().getRole().name()));
+
+        // UserDetails 객체 생성
+        return new org.springframework.security.core.userdetails.User(
+                user.get().getNickname(),
+                user.get().getPassword(),
+                authorities
+        );
     }
 }
