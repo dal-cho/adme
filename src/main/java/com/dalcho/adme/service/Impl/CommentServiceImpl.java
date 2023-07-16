c9c31d8cb7399cb87798bd7022ce88c98cfbd8f8
soyeon <luora@naver.com>
2023-07-16 23:35:14 +0900

    [delete] 필요없는 코드 제거 [#235]

--- src/main/java/com/dalcho/adme/service/Impl/CommentServiceImpl.java
+++ src/main/java/com/dalcho/adme/service/Impl/CommentServiceImpl.java
@@ -24,7 +24,6 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
