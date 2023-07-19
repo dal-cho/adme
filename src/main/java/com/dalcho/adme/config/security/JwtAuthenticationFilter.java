67b04b8144f0fe9b2df0434fa72ab0bc0f3fb2d0
soyeon <luora@naver.com>
2023-07-19 19:41:01 +0900

    [update] oauth & from login [#235]

--- src/main/java/com/dalcho/adme/config/security/JwtAuthenticationFilter.java
+++ src/main/java/com/dalcho/adme/config/security/JwtAuthenticationFilter.java
@@ -26,18 +26,26 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         // servletRequest 에서 token 추출
         String token = jwtTokenProvider.resolveToken(servletRequest);
         log.info("[dofilterInternal] token 값 유효성 체크 시작");
-
-
-        System.out.println(" request uri " + servletRequest.getRequestURI());
-
-        // token 이 유효하다면 Authentication 객체를 생성해서 SecurityContextHolder 에 추가
-        if (token != null && jwtTokenProvider.validateToken(token)) {
-            Authentication authentication = jwtTokenProvider.getAuthentication(token);
-            SecurityContextHolder.getContext().setAuthentication(authentication);
-            log.info("[doFilterInternal] token 값 유효성 체크 완료");
-            return;
-        } else {
-            log.info("[doFilterInternal] 유효한 JWT 토큰이 없습니다, uri: {}", servletRequest.getRequestURL());
+        try{
+            Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+            if(auth.getPrincipal() != null && auth.getCredentials() == null){
+                log.info(" [  oauth  ]   login ");
+                filterChain.doFilter(servletRequest, servletResponse);
+                return; // 필터 종료
+            }else{
+                // token 이 유효하다면 Authentication 객체를 생성해서 SecurityContextHolder 에 추가
+                if (token != null && jwtTokenProvider.validateToken(token)) {
+                    Authentication authentication = jwtTokenProvider.getAuthentication(token);
+                    SecurityContextHolder.getContext().setAuthentication(authentication);
+                    log.info("[doFilterInternal] token 값 유효성 체크 완료");
+                    return;
+                } else {
+                    log.info("[doFilterInternal] 유효한 JWT 토큰이 없습니다, uri: {}", servletRequest.getRequestURL());
+                }
+            }
+        }
+        catch (NullPointerException e){
+            log.info("doFilterInternal");
         }
 
         // 요청 정보가 매칭되지 않을경우 동작
