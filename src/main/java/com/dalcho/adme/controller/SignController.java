fc0c6a30c70efb16d84fe3119ce63a56aa205f6c
Yu_Jehyeop <fbwpguq@gmail.com>
2023-11-30 15:52:30 +0900

    [기능 변경 및 추가] 이메일 및 비밀번호 pattern 오류시 값 반환 & 쿠키 삭제

--- src/main/java/com/dalcho/adme/controller/SignController.java
+++ src/main/java/com/dalcho/adme/controller/SignController.java
@@ -7,16 +7,12 @@ import com.dalcho.adme.dto.sign.SignUpResultDto;
 import com.dalcho.adme.service.SignService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletResponse;
 
 @Slf4j
 @RestController
