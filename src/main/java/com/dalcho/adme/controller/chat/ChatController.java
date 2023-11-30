14f6aa676d8b10e2b9e8618cd1b1bd998bb29d21
soyeon <luora@naver.com>
2023-12-01 00:45:37 +0900

    [update] redis 적용 [#257]

--- src/main/java/com/dalcho/adme/controller/chat/ChatController.java
+++ src/main/java/com/dalcho/adme/controller/chat/ChatController.java
@@ -1,17 +1,26 @@
 package com.dalcho.adme.controller.chat;
 
+import com.dalcho.adme.config.RedisPublisher;
+import com.dalcho.adme.config.RedisSubscriber;
 import com.dalcho.adme.config.security.JwtTokenProvider;
 import com.dalcho.adme.domain.User;
 import com.dalcho.adme.dto.chat.ChatMessage;
+import com.dalcho.adme.dto.chat.ChatMessage.MessageType;
 import com.dalcho.adme.service.Impl.ChatServiceImpl;
 import com.dalcho.adme.service.Impl.RedisService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
+
+import javax.annotation.PostConstruct;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 @RequiredArgsConstructor
@@ -22,20 +31,34 @@ public class ChatController {
 	private final JwtTokenProvider jwtTokenProvider;
 	private final RedisService redisService;
 
+	private final RedisMessageListenerContainer redisMessageListener;
+	private final RedisPublisher redisPublisher;
+	private final RedisSubscriber redisSubscriber;
+	private Map<String, ChannelTopic> channels;
+
+	@PostConstruct
+	public void init(){
+		channels = new HashMap<>();
+	}
 	@MessageMapping("/chat/sendMessage")
 	public void sendMessage(@Payload ChatMessage chatMessage) {
-		template.convertAndSend("/topic/public/" + chatMessage.getRoomId(), chatMessage);
+		ChannelTopic channel = channels.get(chatMessage.getRoomId());
+		redisPublisher.publish(channel, chatMessage);
 	}
 
 	@MessageMapping("/chat/addUser")
 	public void addUser(@Payload ChatMessage chatMessage, SimpMessageHeaderAccessor headerAccessor) {
 		String token = headerAccessor.getFirstNativeHeader("Authorization");
 		User user= jwtTokenProvider.getUserFromToken(token);
+		String roomId = chatMessage.getRoomId();
+		ChannelTopic channel = new ChannelTopic("/topic/public/" + roomId);
+		redisMessageListener.addMessageListener(redisSubscriber, channel);
+		channels.put(roomId, channel);
 		log.info("[chat] addUser token 검사: " + user.getNickname());
 		chatMessage.setSender(user.getNickname());
-		chatMessage.setType(ChatMessage.MessageType.JOIN);
+		chatMessage.setType(MessageType.JOIN);
 		redisService.addRedis(chatMessage);
-		chatService.connectUser("Connect", chatMessage.getRoomId(), chatMessage);
-		template.convertAndSend("/topic/public/" + chatMessage.getRoomId(), chatMessage);
+		chatService.connectUser("Connect", roomId, chatMessage);
+		template.convertAndSend("/topic/public/" + roomId, chatMessage);
 	}
 }
