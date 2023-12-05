cae68babfc73adea010983eda1285ba1b38543a9
soyeon <luora@naver.com>
2023-12-05 18:03:51 +0900

    [update] 코드 수정 [#260]

--- src/main/java/com/dalcho/adme/dto/PagingDto.java
+++ src/main/java/com/dalcho/adme/dto/PagingDto.java
@@ -4,9 +4,7 @@ import lombok.Getter;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @Getter
 public class PagingDto<T> {
