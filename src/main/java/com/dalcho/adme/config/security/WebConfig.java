e0cf521c60aa58a974ac9d79cec3e01e8aeb11fd
Yu_Jehyeop <fbwpguq@gmail.com>
2024-01-10 18:53:54 +0900

    [기능 수정] cors 로직 수정

--- src/main/java/com/dalcho/adme/config/security/WebConfig.java
+++ src/main/java/com/dalcho/adme/config/security/WebConfig.java
@@ -24,7 +24,8 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedMethods(List.of("HEAD", "GET", "POST", "PUT", "DELETE"));
-        config.setAllowedOrigins(List.of("https://www.admee.site","https://api.admee.site"));
+        config.setAllowedOrigins(List.of("https://www.admee.site"));
+        config.setAllowCredentials(true);
 
         registry.addMapping("/**")
                 .combine(config);
