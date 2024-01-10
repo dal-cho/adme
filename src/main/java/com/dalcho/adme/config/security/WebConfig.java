7c9c214f88ca10bb8ffdab14b02d2cb2b28d9f81
Yu_Jehyeop <fbwpguq@gmail.com>
2024-01-10 18:36:14 +0900

    [기능 수정] cors 로직 수정

---
+++ src/main/java/com/dalcho/adme/config/security/WebConfig.java
@@ -0,0 +1,32 @@
+package com.dalcho.adme.config.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+    private final List<HandlerInterceptor> interceptors;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        interceptors.forEach(registry::addInterceptor);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedMethods(List.of("HEAD", "GET", "POST", "PUT", "DELETE"));
+        config.setAllowedOrigins(List.of("https://www.admee.site"));
+
+        registry.addMapping("/**")
+                .combine(config);
+    }
+}
