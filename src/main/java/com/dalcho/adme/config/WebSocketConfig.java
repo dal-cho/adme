d9a96dc8a55c3212e4ee612e67472127627a6a49
soyeon <luora@naver.com>
2024-01-25 23:21:41 +0900

    [update] cors 설정

--- src/main/java/com/dalcho/adme/config/WebSocketConfig.java
+++ src/main/java/com/dalcho/adme/config/WebSocketConfig.java
@@ -22,7 +22,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").setAllowedOriginPatterns("http://api.admee.site").withSockJS();
+        registry.addEndpoint("/ws").setAllowedOriginPatterns("https://api.admee.site").withSockJS();
     }
 
     @Override
