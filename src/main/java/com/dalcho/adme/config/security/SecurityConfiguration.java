f6da08cf929cd22f701504beba1b45bb3ebfaa4d
soyeon <luora@naver.com>
2024-01-25 22:59:42 +0900

    [update] cors 설정

--- src/main/java/com/dalcho/adme/config/security/SecurityConfiguration.java
+++ src/main/java/com/dalcho/adme/config/security/SecurityConfiguration.java
@@ -92,7 +92,7 @@ public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOriginPatterns(List.of("https://www.admee.site"));
+        configuration.setAllowedOriginPatterns(List.of("https://www.admee.site", "https://www.admee.site/*"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         configuration.setAllowedMethods(Arrays.asList("HEAD", "GET", "POST", "PUT", "DELETE"));
         configuration.setAllowCredentials(true);
