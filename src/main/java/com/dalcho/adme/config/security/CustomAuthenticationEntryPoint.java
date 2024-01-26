f2773c6c8a9faffb820a30b79d409514ad7f307c
soyeon <luora@naver.com>
2024-01-26 16:34:48 +0900

    [update] cors 설정

--- src/main/java/com/dalcho/adme/config/security/CustomAuthenticationEntryPoint.java
+++ src/main/java/com/dalcho/adme/config/security/CustomAuthenticationEntryPoint.java
@@ -17,7 +17,6 @@ public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint 
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
         log.info("[ commence ] : " + "인증 실패");
-        response.setHeader("Access-Control-Allow-Origin", "https://www.admee.site");
         //response.sendRedirect("https://www.admee.site/templates/login.html");
     }
 }
