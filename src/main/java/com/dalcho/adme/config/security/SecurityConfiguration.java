7bb03c7ce615a6063515ed117a190c427642444e
soyeon <luora@naver.com>
2024-01-26 18:56:12 +0900

    [update] templates

--- src/main/java/com/dalcho/adme/config/security/SecurityConfiguration.java
+++ src/main/java/com/dalcho/adme/config/security/SecurityConfiguration.java
@@ -39,7 +39,7 @@ public class SecurityConfiguration {
             "/",
             "/oauth2/**",
             "/sign-up",
-            "/templates/**"
+            "templates/**"
     };
 
 @Bean
@@ -93,7 +93,7 @@ public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOriginPatterns(List.of("https://www.admee.site", "https://api.admee.site"));
+        configuration.setAllowedOriginPatterns(List.of("https://www.admee.site"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         configuration.setAllowedMethods(Arrays.asList("HEAD", "GET", "POST", "PUT", "DELETE"));
         configuration.setAllowCredentials(true);
