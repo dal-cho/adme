169072a07d5da3c717cb3da0ceca49d698eb36cb
soyeon <luora@naver.com>
2024-01-28 08:08:40 +0900

    [update] WebSocketDisconnectListener

--- src/main/java/com/dalcho/adme/service/Impl/RedisService.java
+++ src/main/java/com/dalcho/adme/service/Impl/RedisService.java
@@ -14,39 +14,39 @@ import java.util.concurrent.TimeUnit;
 @RequiredArgsConstructor
 @Slf4j
 public class RedisService {
-	private final StringRedisTemplate redisTemplate;
+    private final StringRedisTemplate redisTemplate;
 
-	@Cacheable(key = "#chatMessage.sender", value = "roomId", unless = "#chatMessage.roomId == null")
-	public void addRedis(ChatMessage chatMessage){
-		log.info("[addRedis의 KEY] : " + chatMessage.getSender());
-		long expireTimeInSeconds = 24 * 60 * 60;
-		long creationTimeInMillis = System.currentTimeMillis();
-		long remainingTimeInSeconds = expireTimeInSeconds - ((System.currentTimeMillis() - creationTimeInMillis) / 1000);
-		redisTemplate.opsForValue().set(chatMessage.getSender(), chatMessage.getRoomId(), remainingTimeInSeconds, TimeUnit.SECONDS);
-	}
+    @Cacheable(key = "#chatMessage.sender", value = "roomId", unless = "#chatMessage.roomId == null")
+    public void addRedis(ChatMessage chatMessage) {
+        log.info("[addRedis의 KEY] : " + chatMessage.getSender());
+        long expireTimeInSeconds = 24 * 60 * 60;
+        long creationTimeInMillis = System.currentTimeMillis();
+        long remainingTimeInSeconds = expireTimeInSeconds - ((System.currentTimeMillis() - creationTimeInMillis) / 1000);
+        redisTemplate.opsForValue().set(chatMessage.getSender(), chatMessage.getRoomId(), remainingTimeInSeconds, TimeUnit.SECONDS);
+    }
 
-	@CachePut(value = "roomId", key = "#nickname", unless = "#result == null")
-	public String getRedis(String nickname){
-		return redisTemplate.opsForValue().get(nickname);
-	}
+    @CachePut(value = "roomId", key = "#nickname", unless = "#result == null")
+    public String getRedis(String nickname) {
+        return redisTemplate.opsForValue().get(nickname);
+    }
 
-	public void deleteRedis(String nickname){
-		redisTemplate.delete(nickname);
-	}
+    public void deleteRedis(String nickname) {
+        redisTemplate.delete(nickname);
+    }
 
-	@Cacheable(key = "#email", value = "accessToken", unless = "#result == null || #accessToken == null")
-	public void addToken(String email, String accessToken){
-		long expireTimeInSeconds = 24 * 60 * 60;
-		long creationTimeInMillis = System.currentTimeMillis();
-		long remainingTimeInSeconds = expireTimeInSeconds - ((System.currentTimeMillis() - creationTimeInMillis) / 1000);
-		redisTemplate.opsForValue().set(email, accessToken, remainingTimeInSeconds, TimeUnit.SECONDS);
-	}
+    @Cacheable(key = "#email", value = "accessToken", unless = "#result == null || #accessToken == null")
+    public void addToken(String email, String accessToken) {
+        long expireTimeInSeconds = 24 * 60 * 60;
+        long creationTimeInMillis = System.currentTimeMillis();
+        long remainingTimeInSeconds = expireTimeInSeconds - ((System.currentTimeMillis() - creationTimeInMillis) / 1000);
+        redisTemplate.opsForValue().set(email, accessToken, remainingTimeInSeconds, TimeUnit.SECONDS);
+    }
 
-	@Cacheable(key = "#sessionId", value = "nickname", unless = "#result == null || #accessToken == null")
-	public void addNickname(String sessionId, String nickname){
-		long expireTimeInSeconds = 24 * 60 * 60;
-		long creationTimeInMillis = System.currentTimeMillis();
-		long remainingTimeInSeconds = expireTimeInSeconds - ((System.currentTimeMillis() - creationTimeInMillis) / 1000);
-		redisTemplate.opsForValue().set(sessionId, nickname, remainingTimeInSeconds, TimeUnit.SECONDS);
-
-	}
+    @Cacheable(key = "#sessionId", value = "nickname", unless = "#result == null || #accessToken == null")
+    public void addNickname(String sessionId, String nickname) {
+        long expireTimeInSeconds = 24 * 60 * 60;
+        long creationTimeInMillis = System.currentTimeMillis();
+        long remainingTimeInSeconds = expireTimeInSeconds - ((System.currentTimeMillis() - creationTimeInMillis) / 1000);
+        redisTemplate.opsForValue().set(sessionId, nickname, remainingTimeInSeconds, TimeUnit.SECONDS);
+    }
+}
