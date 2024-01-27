ff4f53eef87d1c9eaa297bf53ee8db52807d54a7
soyeon <luora@naver.com>
2024-01-27 22:54:15 +0900

    [update] sse

--- src/main/java/com/dalcho/adme/config/security/SecurityConfiguration.java
+++ src/main/java/com/dalcho/adme/config/security/SecurityConfiguration.java
@@ -40,7 +40,7 @@ public class SecurityConfiguration {
             "/oauth2/**",
             "/sign-up",
             "/templates/**",
-            "/room/*"
+            "/room/**"
     };
 
 @Bean
