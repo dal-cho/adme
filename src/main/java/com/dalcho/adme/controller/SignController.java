4ee0fc5b1f82b76493c6758557982dde39a57427
soyeon <luora@naver.com>
2024-01-31 01:27:13 +0900

    [update] SignController 수정 [#283]

--- src/main/java/com/dalcho/adme/controller/SignController.java
+++ src/main/java/com/dalcho/adme/controller/SignController.java
@@ -7,16 +7,12 @@ import com.dalcho.adme.dto.sign.SignUpResultDto;
 import com.dalcho.adme.service.SignService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Slf4j
 @RestController
