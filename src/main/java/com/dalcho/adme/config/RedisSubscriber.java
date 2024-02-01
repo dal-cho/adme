34ff44c9e7261e1cdbfe762eb3238c09502413fd
soyeon <luora@naver.com>
2024-02-01 18:40:47 +0900

    [update] 구독자 제거 [#285]

--- src/main/java/com/dalcho/adme/config/RedisSubscriber.java
+++ src/main/java/com/dalcho/adme/config/RedisSubscriber.java
@@ -34,11 +34,6 @@ public class RedisSubscriber implements MessageListener {
             ChatMessage chatMessage = objectMapper.readValue(msg, ChatMessage.class);
             log.info("chatMessage : " + chatMessage);
 
-            try (Jedis jedis = new Jedis("www.admee.site")) {
-                Map<String, String> stringStringMap = jedis.pubsubNumSub(channel);
-                log.info("구독자 수: " + stringStringMap.get(channel));
-            }
-
             if (chatMessage.getType().equals(MessageType.TALK)) {
                 messagingTemplate.convertAndSend(channel, chatMessage);
             }
