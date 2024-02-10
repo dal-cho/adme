850f1eb77ded2024f0a888a5994fb931fe02346b
soyeon <luora@naver.com>
2024-02-10 10:57:16 +0900

    [update] chat

--- src/main/java/com/dalcho/adme/config/security/SecurityConfiguration.java
+++ src/main/java/com/dalcho/adme/config/security/SecurityConfiguration.java
@@ -39,7 +39,8 @@ public class SecurityConfiguration {
             "/tenSeconds",
             "/",
             "/sign-up",
-            "/templates/**"
+            "/templates/**",
+            "/ws/**"
     };
 
 @Bean
