f941474a4573512c1a06e3db6fd664d283ffd9e0
soyeon <luora@naver.com>
2024-04-17 16:10:01 +0900

    [update] 필드 중복 제거 [#296]

--- src/main/java/com/dalcho/adme/dto/chat/ChatRoomDto.java
+++ src/main/java/com/dalcho/adme/dto/chat/ChatRoomDto.java
@@ -1,27 +1,23 @@
 package com.dalcho.adme.dto.chat;
 
-import com.dalcho.adme.domain.Chat;
 import com.dalcho.adme.domain.User;
 import com.dalcho.adme.dto.LastMessage;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.io.Serializable;
-import java.util.List;
 import java.util.UUID;
 
 @Getter
 @Setter
-@ToString
 @Builder
 @AllArgsConstructor
-public class ChatRoomDto implements Serializable { // 일반 crud에서 쓰임
-    private String roomId; // 채팅방 아이디
+public class ChatRoomDto implements Serializable {
+    private String roomId;
     private String nickname;
-    private Integer adminChat;
-    private Integer userChat;
-    private String message;
-    private String day;
-    private String time;
+    private LastMessage lastMessage;
 
     public ChatRoomDto() {
     }
@@ -32,15 +28,10 @@ public class ChatRoomDto implements Serializable { // 일반 crud에서 쓰임
         return room;
     }
 
-    public static ChatRoomDto of(String roomId, User user, LastMessage lastMessage) {
+    public static ChatRoomDto of(User user, LastMessage lastMessage) {
         return ChatRoomDto.builder()
-                .roomId(roomId)
                 .nickname(user.getNickname())
-                .adminChat(lastMessage.getAdminChat())
-                .userChat(lastMessage.getUserChat())
-                .message(lastMessage.getMessage())
-                .day(lastMessage.getDay())
-                .time(lastMessage.getTime())
+                .lastMessage(lastMessage)
                 .build();
     }
 }
